676ae4c8ab6a82a826b1a566707240ec326729a7
Ghassen-Da <ghassendaoud99@gmail.com>
2023-05-02 15:47:00 -0400

    Update test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,4 +1,12 @@
-from ... import metrics
+import os
+import sys
+import inspect
+
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+
+import metrics
 
 print(metrics)
 # content of test_sample.py
