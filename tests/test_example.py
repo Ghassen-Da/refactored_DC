6541ade0658537661c65ca2ae1748d0ed95883e6
Ghassen-Da <ghassendaoud99@gmail.com>
2023-05-02 15:32:31 -0400

    Update test_example.py

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,14 +1,4 @@
-import sys
-sys.path.append('../')
-from metrics import are_significantly_different
-from refactored_DC.metadata import DNNState, InputData
-from refactored_DC.utils import readable
-import data as data
-import refactored_DC.utils as utils
-import refactored_DC.interfaceData as interfaceData
-import refactored_DC.settings as settings
-
-from refactored_DC.settings import CLASSIFICATION_KEY, REGRESSION_KEY
+import refactored_DC.metrics
 
 
 # content of test_sample.py
