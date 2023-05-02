ebd16b47d94172dcecd4e103ac7f2fc7566c2239
Ghassen-Da <ghassendaoud99@gmail.com>
2023-05-02 17:26:59 -0400

    Update test_checkers.py

--- tests/test_checkers.py
+++ tests/test_checkers.py
@@ -317,38 +317,38 @@ def test_biases():
     assert pre_check_biases(initial_biases4, inputs_data2) == '''It is recommended to choose null biases (zeros)'''
  
 
-def pre_check_loss(losses, inputs_data, initial_loss):
-    app_path = Path.cwd()
-    config_fpath = settings.load_user_config_if_exists(app_path)
-    config = settings.Config(config_fpath).pre_check
-    main_msgs = settings.load_messages()
-    rounded_loss_rates = [round(losses[i + 1] / losses[i]) for i in range(len(losses) - 1)]
-    equality_checks = sum(
-        [(loss_rate == config.init_loss.size_growth_rate) for loss_rate in rounded_loss_rates])
-    if equality_checks == len(rounded_loss_rates):
-        return main_msgs['poor_reduction_loss']
-    if inputs_data.problem_type == CLASSIFICATION_KEY:
-        expected_loss = -np.log(1 / inputs_data.targets_metadata['labels'])
-        err = np.abs(initial_loss - expected_loss)
-        if err >= config.init_loss.dev_ratio * expected_loss:
-            return main_msgs['poor_init_loss'].format(readable(err / expected_loss))
+# def pre_check_loss(losses, inputs_data, initial_loss):
+#     app_path = Path.cwd()
+#     config_fpath = settings.load_user_config_if_exists(app_path)
+#     config = settings.Config(config_fpath).pre_check
+#     main_msgs = settings.load_messages()
+#     rounded_loss_rates = [round(losses[i + 1] / losses[i]) for i in range(len(losses) - 1)]
+#     equality_checks = sum(
+#         [(loss_rate == config.init_loss.size_growth_rate) for loss_rate in rounded_loss_rates])
+#     if equality_checks == len(rounded_loss_rates):
+#         return main_msgs['poor_reduction_loss']
+#     if inputs_data.problem_type == CLASSIFICATION_KEY:
+#         expected_loss = -np.log(1 / inputs_data.targets_metadata['labels'])
+#         err = np.abs(initial_loss - expected_loss)
+#         if err >= config.init_loss.dev_ratio * expected_loss:
+#             return main_msgs['poor_init_loss'].format(readable(err / expected_loss))
 
-def test_losses():
-    losses_list0 = [2.56, 4.5, 8.87]
-    losses_list1 = [1, 1.5, 1.6]
-    initial_loss0 = 1.54
-    initial_loss1 = 2.01
-    x_train0 = np.ones((314, 9), dtype=np.int64)
-    y = np.array([[0],[1]])
-    y_train0 = np.repeat(y, [1, 313], axis=0)
-    x_test0 = np.ones((78, 9), dtype=np.int64)
-    y_test0 = np.repeat(y, [1, 77], axis=0)
-    data_loader_under_test = data.DataLoaderFromArrays(x_train0, y_train0, shuffle=True, one_hot=False, target_scaling=True)
-    test_data_loader = data.DataLoaderFromArrays(x_test0, y_test0, shuffle=True, one_hot=False, target_scaling=True)
-    data_to_test = interfaceData.build_data_interface(data_loader_under_test, test_data_loader, homogeneous=True)
-    inputs_data = InputData(data_to_test, 'classification')
-    assert pre_check_loss(losses_list0, inputs_data, initial_loss0) == 'The reduction of the loss is poorly designed: it is recommended to use AVG instead'
-    assert 'Loss at cold start is considered poor and problematic:' in pre_check_loss(losses_list1, inputs_data, initial_loss1)
+# def test_losses():
+#     losses_list0 = [2.56, 4.5, 8.87]
+#     losses_list1 = [1, 1.5, 1.6]
+#     initial_loss0 = 1.54
+#     initial_loss1 = 2.01
+#     x_train0 = np.ones((314, 9), dtype=np.int64)
+#     y = np.array([[0],[1]])
+#     y_train0 = np.repeat(y, [1, 313], axis=0)
+#     x_test0 = np.ones((78, 9), dtype=np.int64)
+#     y_test0 = np.repeat(y, [1, 77], axis=0)
+#     data_loader_under_test = data.DataLoaderFromArrays(x_train0, y_train0, shuffle=True, one_hot=False, target_scaling=True)
+#     test_data_loader = data.DataLoaderFromArrays(x_test0, y_test0, shuffle=True, one_hot=False, target_scaling=True)
+#     data_to_test = interfaceData.build_data_interface(data_loader_under_test, test_data_loader, homogeneous=True)
+#     inputs_data = InputData(data_to_test, 'classification')
+#     assert pre_check_loss(losses_list0, inputs_data, initial_loss0) == 'The reduction of the loss is poorly designed: it is recommended to use AVG instead'
+#     assert 'Loss at cold start is considered poor and problematic:' in pre_check_loss(losses_list1, inputs_data, initial_loss1)
 
 # def pre_check_gradients(numerical, theoretical):
 #     app_path = Path.cwd()
