97858ebd7d06a1398b8b87fe929bc9ee813f45dd
Ghassen-Da <ghassendaoud99@gmail.com>
2023-05-02 15:20:31 -0400

    changes

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,3 +1,14 @@
+from refactored_DC import metrics
+from refactored_DC.metadata import DNNState, InputData
+from refactored_DC.utils import readable
+import data as data
+import refactored_DC.utils as utils
+import refactored_DC.interfaceData as interfaceData
+import refactored_DC.settings as settings
+
+from refactored_DC.settings import CLASSIFICATION_KEY, REGRESSION_KEY
+
+
 # content of test_sample.py
 def func(x):
     return x + 1
