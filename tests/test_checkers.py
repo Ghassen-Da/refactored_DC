4d3a1fcf77c1721c866f9008aa54b95675c7e607
Ghassen-Da <56545375+Ghassen-Da@users.noreply.github.com>
2023-05-03 10:25:48 -0400

    Update test_checkers.py

--- tests/test_checkers.py
+++ tests/test_checkers.py
@@ -11,7 +11,7 @@ import inspect
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
-
+ 
 
 import metrics
 from metadata import InputData
